9cbbe9c77b9786e33de522b232fe01c4974d2379
Amauri Junior <apojr98@gmail.com>
2022-10-17 09:56:26 -0300

    Finish Course

--- src/br/com/alura/bytebank/Principal.java
+++ src/br/com/alura/bytebank/Principal.java
@@ -1,10 +1,28 @@
 package br.com.alura.bytebank;
 
+import br.com.alura.bytebank.io.LeitorCSV;
+import br.com.alura.bytebank.io.LeitorXML;
+import br.com.alura.bytebank.model.Pagamento;
 import br.com.alura.bytebank.visualizador.OpcoesPagamento;
 
+import java.io.IOException;
+import java.util.List;
+
 public class Principal {
-    public static void main(String[] args) {
-        new OpcoesPagamento().mensagemDeBoasVindas();
+    public static void main(String[] args) throws IOException {
+//        new OpcoesPagamento().mensagemDeBoasVindas();
+
+        LeitorCSV leitor = new LeitorCSV();
+        List<Pagamento> pagamentos = leitor.ler(1);
+
+        for (Pagamento pagamento: pagamentos) {
+            double valorBruto = pagamento.getValor();
+            if (valorBruto > 100) {
+                double valorComAcrescimo = valorBruto * 1.1;
+                pagamento.setValor(valorComAcrescimo);
+            }
+            System.out.println(pagamento);
+        }
     }
 
 }
